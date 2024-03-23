f6bc09ff0b445ccc069bef2c1ceb6893477fb28b
hikami <hikami172@gmail.com>
2024-03-23 15:57:08 +0100

    Rearrange settings.py

--- settings.py
+++ settings.py
@@ -1,13 +1,18 @@
 import os
 import logging
+from logging import Logger
 
 import json
 
 import openai
 import spotipy
 
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from google_auth_oauthlib.flow import InstalledAppFlow
 
-ENABLE_DEBUG = True
+
+ENABLE_DEBUG = False
 
 # GUI
 APP_TITLE = "Voice Assistant"
@@ -18,28 +23,54 @@ ICON_PATH = 'assets/icon.ico'
 
 # Logging
 # TODO fix logging settings to show only one source logs
-if ENABLE_DEBUG:
-    logging.basicConfig(level=logging.DEBUG)
-else:
-    logging.basicConfig(level=logging.WARNING)
+level = logging.DEBUG if ENABLE_DEBUG else logging.INFO
+logging.basicConfig(level=level)
 
 # OpenaAI
 # TODO add openai settings
 OPENAI_MODEL = "gpt-3.5-turbo-0613"
-with open("api_keys/openai_api_key", "r") as file:
+with open("api_keys/openai_api_key.json", "r") as file:
     openai_api_key = file.read().strip()
     os.environ["OPENAI_API_KEY"] = openai_api_key
 openai.api_key = os.environ['OPENAI_API_KEY']
 
 # Spotify
 # TODO change the way of setting up spotify client
-SPOTIFY_USERNAME = "Hikami"
-REDIRECT_URI = "http://google.com/callback/"
+SPOTIFY_REDIRECT_URI = "http://google.com/callback/"
 with open("api_keys/spotify_api_key.json", "r") as file:
     keys = json.load(file)
-    CLIENT_ID = keys["client_id"]
-    CLIENT_SECRET = keys["client_secret"]
+    SPOTIFY_CLIENT_ID = keys["client_id"]
+    SPOTIFY_CLIENT_SECRET = keys["client_secret"]
 
-spotify_oauth = spotipy.SpotifyOAuth(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI)
-token_dict = spotify_oauth.get_access_token()
-SPOTIFY_TOKEN = token_dict['access_token']
+# Google APIs
+# scopes = ["https://www.googleapis.com/auth/youtube.force-ssl"]
+#
+# # Set up OAuth 2.0 flow
+# flow = InstalledAppFlow.from_client_secrets_file("api_keys/google_api_key.json", scopes)
+# credentials = flow.run_local_server()
+#
+# # Youtube
+# api_service_name = "youtube"
+# youtube = build(api_service_name, api_version, credentials=credentials)
+# api_version = "v3"
+
+# Programs
+AVAILABLE_PROGRAMS = {
+    'League of Legends': {
+        "executable": r"",
+        "parameters": {
+            "launch-product": "league_of_legends",
+            "launch-patchline": "live",
+        }
+    },
+    'Discord': {
+        "executable": r"",
+        "parameters": {
+            "processStart": "Discord.exe",
+        },
+    },
+    'Spotify': {
+        "executable": r"",
+        "parameters": {}
+    }
+}
