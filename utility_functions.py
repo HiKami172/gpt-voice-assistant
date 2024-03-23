95ceb29dd7954a7eb9ee1f96099280553ad5cc10
hikami <hikami172@gmail.com>
2024-03-23 15:55:21 +0100

    Move excessive functionality to service logic.

--- utility_functions.py
+++ utility_functions.py
@@ -1,11 +1,5 @@
 import json
 import requests
-import webbrowser
-
-import spotipy
-from settings import SPOTIFY_TOKEN
-
-spotifyObject = spotipy.Spotify(auth=SPOTIFY_TOKEN)
 
 
 def get_current_weather(location):
@@ -31,20 +25,4 @@ def get_current_weather(location):
     response = requests.get(url, headers=headers, params=querystring)
 
     return json.dumps(response.json())
-
-
-# TODO finish the function. Remake return value. Add spotify desktop support.
-def spotify_play_song(name):
-    """
-    Search a song by its name and play it in Spotify.
-
-    :param name: A name of the song, e.g. Smells like teen spirit, Californication
-    :return: True if the function worked.
-    """
-    search_result = spotifyObject.search(name, 1, 0, "track")
-    songs_dict = search_result["tracks"]
-    song_items = songs_dict['items']
-    song = song_items[0]['external_urls']['spotify']
-    webbrowser.open(song)
-    return
 
